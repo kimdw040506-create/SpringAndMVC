26371cb1d5e5ee85473ea69c5f45827925dbd59b
kimvvl1 <kimdw040506@gmail.com>
2026-09-10 06:53:30 +0900

    i connected website with code

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -10,11 +10,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "surname")
     private String surname;
 
+    @Column(name = "email")
     private String email;
+
+    public User() {
+    }
 
     public User(String name, String surname, String email) {
         this.name = name;
@@ -22,8 +28,35 @@ public class User {
         this.email = email;
     }
 
-    public Long getId(){
+    public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
